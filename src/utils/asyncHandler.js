ede0eb67d0ecba66b59cc857dea5694a0b04b39b
RecurrciveItu <149315193+RecurrciveItu@users.noreply.github.com>
2024-10-30 08:57:42 +0530

    utill code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+/* HIGHER ORDER FUNCTION IN JS
+const asyncHandler = () => {}
+const asyncHandler = () => {() => {}} OR const asyncHandler = () => () => {}
+cont asyncHandler = () => async () => {}
+ */
+
+// WRAPPER FUNCTION USING TRY CATCH
+
+// const asyncHandler = (fn) => async (err, res, req, next) => {
+//   try {
+//     await fn(req,res,next)
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//         success: false,
+//         message: err.message
+//     });
+//   }
+// };
