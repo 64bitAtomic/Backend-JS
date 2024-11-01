b85778c9050c7491e13e53dd74aec53b52d9e831
RecurrciveItu <149315193+RecurrciveItu@users.noreply.github.com>
2024-11-01 18:42:45 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -46,9 +46,9 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", function (next) {
+userSchema.pre("save", async function (next) {
   if (!this.Modified("password")) return next();
-  this.password = bcrypt(this.password, 10);
+  this.password = await bcrypt(this.password, 10);
   next();
 });
 
